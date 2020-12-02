f9efc7c140a29acbdbf117dc670d135688293954
kohinrichs <ko.hinrichs@gmail.comcdgit config --global user.email ko.hinrichs@gmail.com>
2020-12-02 10:46:32 -0600

    Create harvest module

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,12 +2,14 @@ console.log("Welcome to the main module")
 import { createPlan } from './plan.js'
 import { plantSeeds } from './tractor.js'
 import { usePlants } from './field.js'
+import { harvestPlants } from './harvester.js';
 
 const yearlyPlan = createPlan();
 console.log("This is the yearly plan: ", yearlyPlan);
 
 plantSeeds(yearlyPlan);
-let finalField = usePlants();
+export let finalField = usePlants();
 console.log("This is the final field:", finalField);
 
 
+console.log(harvestPlants(finalField));
